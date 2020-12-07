0df6e92d6f2368221050542e4f55644c62b646e1
John Johnson <jpj9307@gmail.com>
2020-12-07 20:15:24 +0200

    enabled fetching the lists of posts <using mock data>

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,15 +1,30 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+        {
+          _id: '1',
+          title: 'First Post',
+          content: 'This is the first post!',
+          imageUrl: 'images/chopped-kiwi-in-a-bowl-on-a-table.jpg',
+          creator: {
+            name: 'John'
+          },
+          createdAt: new Date()
+        }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
+  const { title, content } = req.body;
+
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content
+    }
   });
 };
