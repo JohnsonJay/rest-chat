0026804a7719d51f2b23c97186dd535d9bdfce0e
John Johnson <jpj9307@gmail.com>
2020-12-08 22:08:38 +0200

    added signup functionality

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
-const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -11,4 +13,25 @@ exports.signup = (req, res, next) => {
     }
     const { email, name, password } = req.body;
 
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email,
+                name,
+                password: hashedPassword,
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({
+                message: 'User created!',
+                userId: result._id
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
