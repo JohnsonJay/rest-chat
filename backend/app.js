c370f7995fba3449ba20fc0c26ecce70c567e2e2
John Johnson <jpj9307@gmail.com>
2020-12-09 20:31:54 +0200

    adding posts and emitting using socket.io

--- backend/app.js
+++ backend/app.js
@@ -62,12 +62,7 @@ app.use((error, req, res, next) => {
 mongoose.connect(process.env.DB_CONNECTION_STRING)
     .then(result => {
         const server = app.listen(8080);
-        const io = require('socket.io')(server, {
-            cors: {
-                origin: 'http://localhost:3000',
-                methods: ['GET', 'POST', 'PUT', 'DELETE']
-            }
-        });
+        const io = require('./socket').init(server);
         io.on('connection', socket => {
            console.log('Client connected');
         });
